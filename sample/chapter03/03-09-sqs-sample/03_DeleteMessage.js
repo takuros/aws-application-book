95c5732c241ed6280e125fd9d377aeefe9f110fe
tenbo07 <tenbo07@gmail.com>
2016-04-16 15:57:04 +0900

    add 03-09-sqs

---
+++ sample/chapter03/03-09-sqs-sample/03_DeleteMessage.js
@@ -0,0 +1,28 @@
+// SDKを読み込み
+var AWS = require("aws-sdk");
+// リージョンを設定
+AWS.config.region = 'ap-northeast-1';
+
+var params = {
+  QueueUrl:  "http://sqs.us-east-1.amazonaws.com/123456789012/queueABC",
+  MaxNumberOfMessages: 1,
+  VisibilityTimeout: 0,
+  WaitTimeSeconds: 0
+};
+
+var sqs = new AWS.SQS();
+sqs.receiveMessage(params, function(err, data) {
+  if (err) console.log(err, err.stack);
+  else {
+    data.Messages.forEach(function(msg) {
+      var params =  {
+        QueueUrl: "http://sqs.us-east-1.amazonaws.com/123456789012/queueABC",
+        ReceiptHandle: msg.ReceiptHandle
+      };
+      sqs.deleteMessage(params, function(err, data) {
+        if (err) console.log(err, err.stack);
+        else     console.log(data);
+      });
+    });
+  }
+});
